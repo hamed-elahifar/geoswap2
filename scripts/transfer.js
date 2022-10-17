4a8980df71adddc3e4c313898ffeacbe390a7fdc
Hamed ElahiFar <hamed.elahifar@gmail.com>
2022-10-17 22:20:25 +0330

    fix transfer

--- scripts/transfer.js
+++ scripts/transfer.js
@@ -10,23 +10,26 @@ const sleepDuration = 10000;
 const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
 
 const to = "0x2AA9EA82AD7aC507401c652086584aE729A24C6D";
+const amount = 1_000;
 
-const transfer = async (contractPath, address, amount) => {
+const transfer = async (contractPath, address) => {
   const contract = await ethers.getContractAt(contractPath, address);
-  const receipt = await contract.transfer(to, amount);
+  const receipt = await contract.transfer(to, BigInt(amount * 10e18));
   await receipt.wait();
-  console.log(`Transfer ${contractPath} successful: ${receipt.hash}`);
+  console.log(
+    `Transfer ${contractPath} (${amount}) successful: ${receipt.hash}`
+  );
   console.log();
 };
 
 const main = async () => {
-  await transfer("WETH", process.env.WETH, 1_000_000);
-  await transfer("A", process.env.A, 1_000_000);
-  await transfer("B", process.env.B, 1_000_000);
-  await transfer("C", process.env.C, 1_000_000);
-  await transfer("DAI", process.env.DAI, 1_000_000);
-  await transfer("USDC", process.env.USDC, 1_000_000);
-  await transfer("USDT", process.env.USDT, 1_000_000);
+  await transfer("WETH", process.env.WETH);
+  await transfer("A", process.env.A);
+  await transfer("B", process.env.B);
+  await transfer("C", process.env.C);
+  await transfer("DAI", process.env.DAI);
+  await transfer("USDC", process.env.USDC);
+  await transfer("USDT", process.env.USDT);
 };
 
 main()
